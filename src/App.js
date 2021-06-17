ba6a7c79665d7aac1f5faea683668db40f5f3391
Mulia Nasution <mul14@users.noreply.github.com>
2021-06-17 10:48:34 +0700

    Add event dispatcher to increase the counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./redux/counter";
 
 function App() {
+  const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
 
   return (
     <div className="App">
       { counter.value }
+      <button onClick={() => dispatch(increment())}>Increase</button>
     </div>
   );
 }
